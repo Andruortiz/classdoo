ccb35e3c64c2c7edff854ba55fa6834a1a7fc3b0
Andruortiz <ortiz.andresdom@gmail.com>
2025-10-10 17:01:43 -0500

    Corresccion en los metodos
    Correcion en los metodos de state y user sqlserver dao

--- ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/sqlserver/StateSqlServerDAO.java
+++ ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/sqlserver/StateSqlServerDAO.java
@@ -1,28 +1,30 @@
 package co.edu.uco.nose.data.dao.entity.sqlserver;
+import co.edu.uco.nose.data.dao.entity.SqlConnection;
 import co.edu.uco.nose.data.dao.entity.StateDAO;
-import co.edu.uco.nose.entity.CountryEntity;
+import co.edu.uco.nose.entity.StateEntity;
 
 import java.sql.Connection;
 import java.util.List;
 import java.util.UUID;
 
-public final class StateSqlServerDAO implements StateDAO{
+public final class StateSqlServerDAO extends SqlConnection implements StateDAO{
 
     public StateSqlServerDAO(final Connection connection) {
         super(connection);
     }
     @Override
-    public List<CountryEntity> findAll() {
+    public List<StateEntity> findAll() {
         return List.of();
     }
 
     @Override
-    public List<CountryEntity> findByFilter(CountryEntity filterEntity) {
+    public List<StateEntity> findByFilter(StateEntity filterEntity) {
         return List.of();
     }
 
+
     @Override
-    public CountryEntity findById(UUID uuid) {
+    public StateEntity findById(UUID uuid) {
         return null;
     }
 }
